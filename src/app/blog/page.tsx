3526d762a348b809b7d3cfacc1372df2383efba7
hanuniye <hanuniye@gmail.com>
2024-05-01 13:05:48 +0300

    initial commit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,16 @@
+import PostCard from "@/components/postCard/PostCard"
+import styles from "./blog.module.css"
 
 const Blog = () => {
   return (
-    <div>Blog Page</div>
+    <div className={styles.container}>
+    {posts.map((post) => (
+      <div className={styles.post} key={post.id}>
+        <PostCard post={post} />
+      </div>
+    ))}
+  </div>
+
   )
 }
 
--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
@@ -29,11 +31,13 @@ const session = true;
 const iaAdmin = true;
 
 const Links = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div className={styles.container}>
       <div className={styles.links}>
         {links.map((link: ILinks, i: number) => {
-          return <NavLink item={link} />;
+          return <NavLink item={link} key={i} />;
         })}
 
         {session ? (
@@ -45,6 +49,15 @@ const Links = () => {
           <NavLink item={{ path: "/login", title: "Login" }} />
         )}
       </div>
+
+      <button className={styles.menuButton} onClick={() => setIsOpen((prev) => !prev)}>Menu</button>
+      {isOpen && (
+        <div className={styles.mobileLinks}>
+          {links.map((link: ILinks, i: number) => {
+            return <NavLink item={link} key={i} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
